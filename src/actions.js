e838c73c36fe5d4c4df74b86278c56c364e052ab
NeilVicente <neil@neilacero.com>
2018-02-03 01:14:28 +0000

    add action creators

---
+++ src/actions.js
@@ -0,0 +1,29 @@
+import C from './constants'
+
+export const addTodo = todo => ({
+  type: C.ADD_TODO,
+  payload: todo
+})
+
+export const removeTodo = index => ({
+  type: C.REMOVE_TODO,
+  payload: index
+})
+
+export const checkTodo = index => ({
+  type: C.CHECK_TODO,
+  payload: index
+})
+
+export const uncheckTodo = index => ({
+  type: C.UNCHECK_TODO,
+  payload: index
+})
+
+export const hideCompleted = () => ({
+  type: C.HIDE_COMPLETED,
+})
+
+export const showCompleted = () => ({
+  type: C.SHOW_COMPLETED,
+})
