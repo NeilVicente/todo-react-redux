6809ae8299ec054e3438d3cecf681882767b1a88
NeilVicente <neil@neilacero.com>
2018-02-03 01:18:01 +0000

    apply console logger middleware to store factory

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,23 @@
 import appReducer from './reducers'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
-export default (initialState = {}) => createStore(appReducer, initialState)
+const consoleMesssages = store => next => action => {
+  let result
+  
+  console.groupCollapsed(`dispatching action => ${action.type}`)
+  console.log('todos', store.getState().allTodos.length)
+  result = next(action)
+
+  let { allTodos, showCompleted } = store.getState()
+
+  console.log(`
+      todos: ${allTodos.length}
+      showCompleted: ${showCompleted}
+  `)
+  console.groupEnd()
+  return result
+}
+
+export default (initialState = {}) => {
+  return applyMiddleware(consoleMesssages)(createStore)(appReducer, initialState)
+}
