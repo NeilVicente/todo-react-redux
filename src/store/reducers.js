234f8d159995e5b33a85c3bbc08180e7d16e134e
NeilVicente <neil@neilacero.com>
2018-02-03 01:13:26 +0000

    add reducers

---
+++ src/store/reducers.js
@@ -0,0 +1,49 @@
+import C from '../constants'
+import { combineReducers } from 'redux'
+
+export  const todo = (state = null, action) => {
+  return action.type === C.ADD_TODO ?
+    action.payload:
+    state
+}
+
+export const allTodos = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_TODO:
+      const hasTodo = state.some(todo => todo.text.trim().toLowerCase() === action.payload.text.trim().toLowerCase());
+      return (hasTodo) ?
+        state:
+        [
+          ...state,
+          todo(null, action)
+        ]
+    case C.REMOVE_TODO:
+      return state.filter((todo, index) => index !== action.payload)
+    case C.CHECK_TODO:
+    case C.UNCHECK_TODO:
+      const updatedTodos = state.map((todo, index) => {
+        if (index === action.payload) {
+          return { ...todo, completed: action.type === C.CHECK_TODO }
+        }
+        return todo
+      })
+      return updatedTodos
+    default:
+      return state
+  }
+}
+
+export const toggleCompleted = (state = true, action) => {
+  if (action.type === C.SHOW_COMPLETED) {
+    return true;
+  } else if ((action.type) === C.HIDE_COMPLETED) {
+    return false
+  } else {
+    return state
+  }
+}
+
+export default combineReducers({
+  allTodos,
+  showCompleted: toggleCompleted
+})
